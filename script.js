658573fe160a88cd182842bee10a974422bdd7fb
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-04 16:58:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function(){
-	let para = document.createElement('p');
-	para.textContent = "DOM load success";
-	document.body.appendChild(para);
-})
+document.addEventListener('DOMContentLoaded', function() {
+  // Create a new paragraph element
+  var paragraph = document.createElement('p');
+  
+  // Set the text content of the paragraph
+  paragraph.textContent = 'DOM load success';
+  
+  // Append the paragraph to the body
+  document.body.appendChild(paragraph);
+});
