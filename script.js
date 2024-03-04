3400721886ed718597313dbf3e980f03d8b67b5d
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-04 17:01:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Create a new paragraph element
-  var paragraph = document.createElement('p');
-  
-  // Set the text content of the paragraph
-  paragraph.textContent = 'DOM load success';
-  
-  // Append the paragraph to the body
-  document.body.appendChild(paragraph);
+  let body = document.querySelector("body");
+	body.textContent = "DOM load success";
 });
