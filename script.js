64c95cabbed65c47bc8af6ecf8b1f21a50d7d938
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-04 16:55:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function(){
+	let para = document.createElement('p');
+	para.textContent = "DOM load success";
+	document.body.appendChild(para);
+})
